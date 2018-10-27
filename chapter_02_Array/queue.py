2d0805686089806b05fdd3f95e7760d081a45a2d
Zhe Wang <wangzhe.dut@gmail.com>
2018-10-27 13:55:06 -0700

    finish chapter 03

--- chapter_02_Array/queue.py
+++ chapter_02_Array/queue.py
@@ -93,24 +93,49 @@ class LoopQueue(QueueBase):
 
 
 if __name__ == '__main__':
-    queue = LoopQueue()
-    queue.enqueue(1)
-    queue.enqueue(4)
-    queue.enqueue(3)
-    queue.enqueue(0)
-    queue.enqueue(10)
-    queue.enqueue(10)
-    queue.enqueue(10)
-    queue.enqueue(10)
-    print(queue)
+    # queue = LoopQueue()
+    # queue.enqueue(1)
+    # queue.enqueue(4)
+    # queue.enqueue(3)
+    # queue.enqueue(0)
+    # queue.enqueue(10)
+    # queue.enqueue(10)
+    # queue.enqueue(10)
+    # queue.enqueue(10)
+    # print(queue)
 
-    queue.dequeue()
-    queue.enqueue(3)
-    print(queue)
-    queue.dequeue()
-    print(queue)
-    queue.dequeue()
-    queue.enqueue(3)
-    print(queue)
-    queue.dequeue()
-    print(queue)
+    # queue.dequeue()
+    # queue.enqueue(3)
+    # print(queue)
+    # queue.dequeue()
+    # print(queue)
+    # queue.dequeue()
+    # queue.enqueue(3)
+    # print(queue)
+    # queue.dequeue()
+    # print(queue)
+
+    from time import time
+    from random import randint
+
+    def test_enqueue(queue, op_count):
+        start_time = time()
+        for i in range(op_count):
+            queue.enqueue(randint(1, 2000))
+        return time() - start_time
+
+    def test_dequeue(queue, op_count):
+        start_time = time()
+        for i in range(op_count):
+            queue.dequeue()
+        return time() - start_time
+
+    op_count = 10000
+    array_queue = ArrayQueue()
+    loop_queue = LoopQueue()
+
+    print('ArrayQueue enqueue: ', test_enqueue(array_queue, op_count))
+    print('LoopQueue enqueue: ', test_enqueue(loop_queue, op_count))
+
+    print('ArrayQueue dequeue: ', test_dequeue(array_queue, op_count))
+    print('LoopQueue dequeue: ', test_dequeue(loop_queue, op_count))
